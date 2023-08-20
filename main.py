d45429efbc63728d4368b633083e1fc82f3c5682
Denis Ustinow <ml_engineer@icloud.com>
2023-08-20 15:17:52 +0700

    Add type annotations for improved code clarity and static analysis

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from phonebook.console_interface import ConsoleInterface
-from phonebook.text_db import TextDB
-
+from phonebook.text_db import TextDB, DataAccessObject
 
 if __name__ == "__main__":
-    db = TextDB()
+    db: DataAccessObject = TextDB()
     db.connect('database.txt')
-    console_interface = ConsoleInterface(db)
+    console_interface: ConsoleInterface = ConsoleInterface(db)
     console_interface.run()
+
